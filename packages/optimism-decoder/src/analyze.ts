9db933f6a37a6891cdf58c5d2d46a6e5aa6fd45d
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2024-01-08 14:00:33 +0100

    add more chains

--- packages/optimism-decoder/src/analyze.ts
+++ packages/optimism-decoder/src/analyze.ts
@@ -10,6 +10,8 @@ const ctcMapping: Record<string, string | undefined> = {
   '0xFf00000000000000000000000000000000008453': 'Base',
   '0x6F54Ca6F6EdE96662024Ffd61BFd18f3f4e34DFf': 'Zora',
   '0xC1B90E1e459aBBDcEc4DCF90dA45ba077d83BFc5': 'PGN',
+  '0xFF00000000000000000000000000000000000010': 'OPMainnet',
+  '0x253887577420Cb7e7418cD4d50147743c8041b28': 'Aevo'
 }
 
 const typeMapping: Record<string, string | undefined> = {
@@ -17,6 +19,8 @@ const typeMapping: Record<string, string | undefined> = {
   Base: 'OpStack',
   Zora: 'OpStack',
   PGN: 'OpStack',
+  OPMainnet: 'OpStack',
+  Aevo: 'OpStack',
   'Boba Network': 'OVM 2.0',
   'Optimism OVM 1.0': 'OVM 1.0',
 }
